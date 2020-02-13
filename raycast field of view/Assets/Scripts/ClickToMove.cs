e7b909991186e84b020580dd52150136b5d6a2eb
Aristeus <jornjellema@hotmail.com>
2020-02-13 19:37:41 +0100

    Base for Field of view
    We can use this base or another one.

---
+++ raycast field of view/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour
+{
+    public float moveSpeed;
+    public CharacterController controller;
+    private Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //Locate where the player clicked on the terrain
+            locatePosition();
+        }
+        //Move the player to the position
+        moveToPosition();
+    }
+
+    void locatePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            //Debug.Log(position);
+        }
+    }
+
+    
+    void moveToPosition()
+    {
+        if (Vector3.Distance(transform.position, position) > 1.2)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(position - transform.position);
+
+            newRotation.x = 0f;
+            newRotation.z = 0f;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
+            controller.SimpleMove(transform.forward * moveSpeed);
+        }
+    }
+    
+}
---
+++ raycast field of view/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public GameObject[] others;
+    [Range(-1, 1)]
+    public float FOV = 0.5f;
+    public float visionRange = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find others
+        others = GameObject.FindGameObjectsWithTag("Target");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calculates angle from self to each target, and normalizes it. (making it between -1 and 1 max)
+        Vector3 a = this.transform.forward;
+        a.Normalize();
+        foreach (GameObject other in others)
+        {
+            //If it finds itself in the array, skip.
+            if (other == this.gameObject) continue;
+
+            Vector3 b = other.transform.position - this.transform.position;
+            b.Normalize();
+            float dot = Vector3.Dot(a, b);
+            dot = Vector3.Dot(a, b);
+
+            //Shoots a raycast to the target if it is in range, in it's field of View and if it has the tag "Target".
+            //If you want you can seperate this line into different 'if' statements.
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, b, out hit, visionRange) && dot > FOV && hit.transform.tag == "Target")
+            {
+                //======Put what you want to happen if a target is in it's field of view here=======================
+
+                //==================================================================================================
+                Debug.Log("hit" + other.name);
+                Debug.DrawRay(transform.position, b * hit.distance, Color.green, 0.1f);
+            } 
+        }     
+    }
+}
